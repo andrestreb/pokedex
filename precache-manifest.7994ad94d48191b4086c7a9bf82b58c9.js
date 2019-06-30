618fcf974fbdc8d2c245ffa197b7a619152ccc98
andrestreb <andrestreb@hotmail.com>
2019-06-30 23:26:29 +0200

    Updates

---
+++ precache-manifest.7994ad94d48191b4086c7a9bf82b58c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63db6f0202f924544ce5880d566b0d5",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ea8d7041635271cdf140",
+    "url": "/pokedex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pokedex/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ea8d7041635271cdf140",
+    "url": "/pokedex/static/js/main.133a41a3.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
