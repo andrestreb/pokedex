e2a1c2f09f9d279a0dd61cc273ef568a5ee42bfa
andrestreb <andrestreb@hotmail.com>
2019-07-08 23:57:03 +0200

    add icons

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import "typeface-roboto";
 import Button from "@material-ui/core/Button";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>Let's change the world!</p>
         <Button variant="contained" color="primary">
